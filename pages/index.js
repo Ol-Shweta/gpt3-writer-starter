e04c84b193cbb9e345e71d34524d5c00dceb18a4
Ol-Shweta <84391338+Ol-Shweta@users.noreply.github.com>
2023-05-04 17:18:12 +0530

    latest build

--- pages/index.js
+++ pages/index.js
@@ -63,11 +63,11 @@ const callGenerateEndpoint = async () => {
   
     {apiOutput && (
   <div className="output">
-    <div className="output-header-container">
+   {/*  <div className="output-header-container">
       <div className="output-header">
         <h3>Output</h3>
       </div>
-    </div>
+    </div> */}
     <div className="output-content">
       <p>{apiOutput}</p>
     </div>
